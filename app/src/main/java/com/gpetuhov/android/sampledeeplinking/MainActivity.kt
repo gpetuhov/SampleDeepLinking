6e88ebdd03041f869e8ecee4eb021c47c97acf3b
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-26 16:56:43 +0300

    Show link if not null

--- app/src/main/java/com/gpetuhov/android/sampledeeplinking/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampledeeplinking/MainActivity.kt
@@ -2,11 +2,15 @@ package com.gpetuhov.android.sampledeeplinking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // This shows link if not null
+        text_view.text = intent?.data?.toString() ?: "Hello World!"
     }
 }
